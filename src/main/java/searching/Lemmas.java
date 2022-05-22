a448f228a8509283c0a5c0f198f4f80c8eef028e
Fenris <anatolijv236@gmail.com>
2022-05-22 15:33:39 +0300

    remove trash words from query

--- src/main/java/searching/Lemmas.java
+++ src/main/java/searching/Lemmas.java
@@ -1,7 +1,6 @@
 package searching;
 
 import entities.Lemma;
-import lombok.AllArgsConstructor;
 import morphology.Morphology;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,12 +10,19 @@ import java.sql.Statement;
 import java.util.Set;
 import java.util.TreeSet;
 
-@AllArgsConstructor
 class Lemmas {
+    private final static double JUNK_PERCENT = 0.9;
     private final Morphology morphology = new Morphology();
 
     private final String searchRequest;
     private final Statement statement;
+    private final int junkLevel;
+
+    public Lemmas(String searchRequest, @NotNull Statement statement) throws SQLException {
+        this.searchRequest = searchRequest;
+        this.statement = statement;
+        junkLevel = getJunkLevel(statement);
+    }
 
     @NotNull Set<Lemma> getLemmas() throws SQLException {
         Set<String> wordsInRequest = morphology.countWords(searchRequest).keySet();
@@ -31,14 +37,23 @@ class Lemmas {
         return lemmas;
     }
 
+    private int getJunkLevel(@NotNull Statement statement) throws SQLException {
+        String sql = "SELECT COUNT(id) count FROM pages";
+        ResultSet result = statement.executeQuery(sql);
+        result.next();
+        int pagesCount = result.getInt("count");
+        return (int) (pagesCount * JUNK_PERCENT);
+    }
+
     private @NotNull String getLemmasSql(@NotNull Set<String> wordsInRequest) {
         StringBuilder sql = new StringBuilder();
-        wordsInRequest.forEach(word -> sql.append(sql.isEmpty() ? "WHERE " : "OR ")
+        wordsInRequest.forEach(word -> sql.append(sql.isEmpty() ? "WHERE (" : "OR ")
                 .append("lemma = '")
                 .append(word)
                 .append("' ")
         );
 
+        sql.append(") AND frequency < ").append(junkLevel);
         sql.insert(0, "SELECT id, lemma, frequency FROM lemmas ");
         return sql.toString();
     }
